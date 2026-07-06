30ba9ba04b2a01cc21ba8b5fed3f30451f730797
loppzh <hugomanoelsl23@gmail.com>
2026-07-06 09:32:55 -0300

    Revert "fix: altera banco de dados para PostgreSQL"
    This reverts commit b514c9787cf914ca7a685bb43931a90839a41a9c.

--- core/settings.py
+++ core/settings.py
@@ -50,12 +50,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'app_db'),
-        'USER': os.environ.get('DB_USER', 'admin'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'admin123'),
-        'HOST': os.environ.get('DB_HOST', 'app-db-svc'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
